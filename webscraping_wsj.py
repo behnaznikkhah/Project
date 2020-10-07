6328e1dedd317e7ccd98b75da184e00131540c38
unknown <behnaz.nikkhah@gmail.com>
2020-10-07 12:11:04 -0400

    add pagination and importing output to a csv file

--- webscraping_wsj.py
+++ webscraping_wsj.py
@@ -1,65 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  2 16:37:29 2020
-
-@author: Behnaz
-"""
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import requests
 import pandas as pd
-import time
-from time import sleep
+from datetime import datetime
+
+
+
 
 PATH = "chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-year=[2020]
-month=[1]
-day=[2]
-headlines=[]
-count=0
 
-driver = webdriver.Chrome('chromedriver.exe')
+year = [2020]
+month = [1]
+day = [1]
+headlines = []
+count = 0
+date=[]
+
 for y in year:
     for m in month:
         for d in day:
-            url=("https://www.wsj.com/news/archive/%i/%i/%i"%(y,m,d))
-            driver.get(url)
-            try:
-                driver.find_element_by_xpath('//*[@id="main"]/div[1]/div')
-                for i in range(1,50):
+                url = ("https://www.wsj.com/news/archive/%i/%i/%i" % (y, m, d))
+                driver.get(url)
+                page=driver.find_element_by_xpath('//*[@id="main"]/div[2]/div[1]/div/div/span')
+                if int(page.text[3]) == 1:
                     try:
-                        news_headlines = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div/ol/article['+ str(i) + ']/div[3]/div/h2')
-                        headlines.append(news_headlines.text)
+                        driver.implicitly_wait(20)
+                        driver.find_element_by_xpath('//*[@id="main"]/div[1]/div')
+                        for i in range(1, 51):
+                            news_headlines = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div/ol/article[' + str(i) + ']/div[3]/div/h2')
+                            headlines.append(news_headlines.text)
+                            da=datetime(year=y,month=m,day=d)
+                            date.append(da)
                     except Exception as e:
                         print(e)
-            except Exception as e:
-                print(e)
-print(headlines)                
+                else :
+                    try:
+                        for p in range (1,(int(page.text[3])+1)):
+                            url2=("https://www.wsj.com/news/archive/%i/%i/%i?page=%i" % (y, m, d,p))
+                            driver.get(url2)
+                            driver.implicitly_wait(20)
+                            driver.find_element_by_xpath('//*[@id="main"]/div[1]/div')
+                            for i in range(1, 51):
+                                news_headlines = driver.find_element_by_xpath('//*[@id="main"]/div[1]/div/ol/article[' + str(i) + ']/div[3]/div/h2')
+                                headlines.append(news_headlines.text)
+                                da=datetime(year=y,month=m,day=d)
+                                date.append(da)
+                    except Exception as e:
+                        print(e)
                 
+                df=pd.DataFrame({"headlines":headlines,"date":date})
                 
-       
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+                    
+print(headlines)
+df.to_csv("output.csv")
